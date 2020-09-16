484dbac67f8c6fc7f0fa4afd2a2dcb474fd5294c
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-09-16 23:20:17 +0300

    optimisation

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,4 @@
 $(function(){
-
-//----start menu-------
 $('.btn__menu').click(function(){
 	$(this).toggleClass('active');
 	$('.header').toggleClass('active');
@@ -10,9 +8,6 @@ $('.header li').click(function(){
 	$('.btn__menu').removeClass('active');
 
 })
-//----end menu----------
-
-// ------start sticky header------
 $(window).scroll(function() {
 if ($(this).scrollTop() > 1){
 $('.header').addClass("sticky");
@@ -21,63 +16,13 @@ else{
 $('.header').removeClass("sticky");
 }
 });
-//-------end sticky header--------
-
-//-------start img rotate-------
-// setInterval(function() {
-// 	$('.main-screen__img-wrapper').toggleClass('active');
-// }, 5000);
-// start slider
 $('.slider').slick({
 prevArrow: false,
 nextArrow: false,
 slidesToShow: 1,
 dots: true
 });
-// end slider
-//------end img rotate------
-
-//------start popup------
-$('.main-screen__video-link').click(function(){
-	$('.video-popup').addClass('active');
-})
-$('.video-popup__close').click(function(){
-	$('.video-popup').removeClass('active');
-})
-jQuery(function($){
-  $(document).mouseup(function (e){ 
-    var div = $(".video-popup-wrapper"); 
-    var div2 = $(".video-popup");
-    if (!div.is(e.target) 
-        && div.has(e.target).length === 0) {
-      div2.removeClass('active');
-    }
-  });
 });
-//------end popup-------
-});
-
-//start captcha//
-
-function scaleCaptcha(elementWidth) {
-  var reCaptchaWidth = 304;
-    var containerWidth = $('.container2').width();
-  if(reCaptchaWidth > containerWidth) {
-    var captchaScale = containerWidth / reCaptchaWidth;
-    $('.g-recaptcha').css({
-      'transform':'scale('+captchaScale+')'
-    });
-  }
-}
-
-$(function() { 
-  scaleCaptcha();
-  $(window).resize( $.throttle( 100, scaleCaptcha ) );
-  
-});
-//end captche 
-
-//start cookies popup
 setTimeout(function(){
   $('.cookies-popup').addClass('active');
 }, 5000);
@@ -85,6 +30,4 @@ setTimeout(function(){
 $('.cookies-popup .button').click(function(){
   $('.cookies-popup').hide();
 })
-//end cookies popup 
-
-
+$.lazyLoadXT.onload.addClass = "animated fadeIn";
